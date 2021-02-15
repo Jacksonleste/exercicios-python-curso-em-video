57374b1b4faa551ef68fd3d213776cf1ce6e6482
Jacksonleste <75914736+Jacksonleste@users.noreply.github.com>
2021-02-14 23:41:11 -0300

    Delete es065.py

--- es065.py
+++
@@ -1,9 +0,0 @@
-pross = 'S'
-cont = soma = 0
-while pross == 'S':
-    n = int(input('insira um número:'))
-    pross = str(input('quer digitar mais um número [S/N]?')).strip().upper()
-    cont += 1
-    soma += n
-    media = soma / cont
-print('a média dos {} números digitados é {}'.format(cont, media))
