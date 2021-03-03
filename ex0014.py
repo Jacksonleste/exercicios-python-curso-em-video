d613b5329d8066c6c2fa5d18fb5345669014c8f0
Jackson Leste <jacksongoncalves.leste@gmail.com>
2021-03-02 21:03:37 -0300

    rename

--- ex0014.py
+++
@@ -1,4 +0,0 @@
-cel = float(input('\033[30;1mqual a temperatura em C°?'))
-far = 1.8*cel+32
-ke = cel+273.15
-print(' a tempreatura \033[31m{}C°\033[30m equivale a \033[36m{:.2f}F°\033[30m e \033[36m{}K°\033[m '.format(cel, far, ke))
